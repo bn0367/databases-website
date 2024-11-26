c0c0a9f48a97d71b4852b479ace9e4053b6e19f9
﻿bn0367 <benbensmail@gmail.com>
2024-11-26 16:43:20 -0500

    living on the edge

--- index.php
+++ index.php
@@ -4,25 +4,33 @@ global $serverName, $dbName, $user, $pw;
 include "vars.php";
 
 try {
+    print("<!DOCTYPE html><html lang='en'><head><title>View Table</title><link rel='stylesheet' href='style.css'></head><body><a href='index.html'>Back</a>");
+    $table = $_POST['table'];
+
+    if (!in_array($table, ["artwork", "artist", "dimensions", "metadata"])) {
+        print("Invalid table name.</body></html>");
+        die();
+    }
+
     $conn = new PDO("pgsql:host=$serverName;dbname=$dbName", $user, $pw);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $stmt = $conn->prepare("SELECT * FROM artwork LIMIT 100");
-    $stmt->execute();
-    print("<table style='border: 1px solid black;border-collapse: collapse'><tr>");
+    $stmt = $conn->prepare("SELECT * FROM :table LIMIT 100");
+    $stmt->execute(array(":table" => $table));
+    print("<table><tr>");
     for ($i = 0; $i < $stmt->columnCount(); $i++) {
         $col = $stmt->getColumnMeta($i);
-        print("<th style='font-weight: bold'>" . $col["name"] . "</th>");
+        print("<th>" . $col["name"] . "</th>");
     }
     print("</tr>");
     foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $row) {
-        print("<tr style='border: 1px solid black;border-collapse: collapse'>");
+        print("<tr>");
         foreach ($row as $key => $value) {
             print("<td>" . $value . "</td>");
         }
         print("</tr>");
     }
-    print("</table>");
+    print("</table></body></html>");
 } catch (PDOException $e) {
     print("Internal Server Error: " . $e->getMessage());
 }
