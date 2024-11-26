484f0848b2cad854587f58aad86348bb9831a02b
﻿bn0367 <benbensmail@gmail.com>
2024-11-26 17:10:46 -0500

    link to root instead of file

--- db.php
+++ db.php
@@ -4,7 +4,7 @@ global $serverName, $dbName, $user, $pw;
 include "vars.php";
 
 try {
-    print("<!DOCTYPE html><html lang='en'><head><title>View Table</title><link rel='stylesheet' href='style.css'></head><body><a href='index.html'>Back</a><br/>");
+    print("<!DOCTYPE html><html lang='en'><head><title>View Table</title><link rel='stylesheet' href='style.css'></head><body><a href='/'>Back</a><br/>");
     $table = $_POST['table'];
 
     if (!in_array($table, ["artwork", "artist", "dimensions", "metadata"])) {
