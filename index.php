aa35d3d8bf250f1eb662767051d8e16c8671fd25
﻿bn0367 <benbensmail@gmail.com>
2024-11-26 14:52:49 -0500

    test get artwork table

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+global $serverName, $dbName, $user, $pw;
+include "vars.php";
+
+try {
+    $conn = new PDO("pgsql:host=$serverName;dbname=$dbName",
+        $user, $pw);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $conn->prepare("SELECT * FROM artwork LIMIT 100");
+    $stmt->execute();
+
+    print("<table>");
+    foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $row) {
+        print("<tr><td>");
+        foreach ($row as $key => $value) {
+            print($value);
+        }
+        print("</td></tr>");
+    }
+    print("</table>");
+} catch (PDOException $e) {
+    print("Internal Server Error.");
+}
---
+++ vars.php
@@ -0,0 +1,9 @@
+<?php
+
+global $serverName, $dbName, $user, $pw;
+
+$serverName = "http://database-postgresql.cs311.svc.cluster.local";
+$dbName = "art";
+$user = "postgres";
+$pw = getenv("postgres-password");
+$userID = 38;
