7703830b53c170f14729b6e71a9c617f756345c3
﻿bn0367 <benbensmail@gmail.com>
2024-12-15 16:33:08 -0500

    add art page php script

---
+++ art/art.php
@@ -0,0 +1,38 @@
+<?php
+
+global $serverName, $dbName, $user, $pw;
+include "../vars.php";
+
+try {
+    if (!isset($_POST['id'])) {
+        print("{status: 'error', error: 'No id provided!'}");
+        die();
+    }
+    $conn = new PDO("pgsql:host=$serverName;dbname=$dbName", $user, $pw);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $conn->prepare("SELECT artwork.id, title, name as artist, date_range as date, culture, category, type, materials FROM artwork JOIN artist ON artwork.artist_id = artist.id JOIN metadata ON artwork.id = metadata.id WHERE artwork.id = :id");
+    $stmt->execute(array(
+        "id" => $_POST['id'],
+    ));
+
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    print("{\"status\": \"success\", \"objects\": [");
+    $row_strs = [];
+    foreach ($rows as $row) {
+        $row_str = "{";
+        $values = [];
+        foreach ($row as $key => $value) {
+            $escaped_value = addslashes($value);
+            $escaped_key = addslashes($key);
+            $values[] = "\"$key\": \"$value\"";
+        }
+        $row_str .= implode(",", $values);
+        $row_str .= "}";
+        $row_strs[] = $row_str;
+    }
+    print(implode(",", $row_strs));
+    print("]}");
+} catch (PDOException $e) {
+    print("Internal Server Error: " . $e->getMessage());
+}
